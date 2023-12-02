e0acbd8ec6e297c84d16fb3ad51646bec319f459
itachi1010 <onahben977@gmail.com>
2023-12-02 01:37:22 -1100

    adding the admin download functionality

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 # users/urls.py
 
 from django.urls import path
-from .views import notifications, send_notification, register,profile_update
+from .views import notifications, send_notification, register, profile_update, admin_page, user_detail, \
+    download_user_info
 
 urlpatterns = [
     # Your existing URL patterns
@@ -9,5 +10,8 @@ urlpatterns = [
     path('notifications/', notifications, name='notifications'),
     path('send_notification/<int:user_id>/', send_notification, name='send_notification'),
     path('profile/update/', profile_update, name='profile_update'),
+    path('admin_page/', admin_page, name='admin_page'),
+    path('user_detail/<int:user_id>/', user_detail, name='user_detail'),
+    path('download_user_info/<int:user_id>/', download_user_info, name='download_user_info'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,16 @@
 # users/views.py
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
 from .form import UserRegisterForm, UserUpdateForm, ProfileUpdateForm, AdminNotificationForm
 from .models import Notification, Profile
+from django.http import HttpResponse
+from django.core.files import File
+import json
+import zipfile
+import os
+from io import BytesIO
 
 def register(request):
     if request.method == 'POST':
@@ -59,3 +65,56 @@ def profile_update(request):
     else:
         form = ProfileUpdateForm(instance=request.user.profile)
     return render(request, 'users/profile_update.html', {'form': form})
+
+@login_required
+@user_passes_test(lambda u: u.is_superuser)
+def admin_page(request):
+    users = User.objects.all()
+    return render(request, 'users/admin_page.html', {'users': users})
+
+@login_required
+@user_passes_test(lambda u: u.is_superuser)
+def user_detail(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    return render(request, 'users/user_detail.html', {'user': user})
+
+
+
+@login_required
+@user_passes_test(lambda u: u.is_superuser)
+def download_user_info(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+
+    # Prepare user data
+    user_data = {
+        'username': user.username,
+        'email': user.email,
+        'phone_number': user.profile.phone_number,
+        # ... add more fields as needed ...
+    }
+
+    # Prepare JSON file
+    json_data = json.dumps(user_data, indent=2)
+    json_file = BytesIO(json_data.encode())
+    json_filename = f'{user.username}_info.json'
+
+    # Prepare ZIP file
+    zip_filename = f'{user.username}_info_and_images.zip'
+    zip_buffer = BytesIO()
+
+    with zipfile.ZipFile(zip_buffer, 'w') as zip_file:
+        # Add JSON file to the ZIP
+        zip_file.writestr(json_filename, json_data)
+
+        # Add user images to the ZIP
+        for image_field in ['id_front_image', 'id_back_image', 'selfie_with_id_image']:
+            image = getattr(user.profile, image_field)
+            if image:
+                image_name = os.path.basename(image.name)
+                zip_file.write(image.path, arcname=image_name)
+
+    # Create HTTP response with ZIP file
+    response = HttpResponse(zip_buffer.getvalue(), content_type='application/x-zip-compressed')
+    response['Content-Disposition'] = f'attachment; filename="{zip_filename}"'
+    return response
+
