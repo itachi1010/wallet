e6a2d7a38dbf27e771bc06678e03e66054d2df28
itachi1010 <onahben977@gmail.com>
2023-12-02 00:40:33 -1100

    adding the admin download functionality

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,15 @@
 # users/admin.py
 
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
 from .models import Profile, Notification
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'wallet', 'image']
@@ -20,5 +28,7 @@ class NotificationAdmin(admin.ModelAdmin):
     list_display = ['user', 'message', 'timestamp', 'is_read']
     search_fields = ['user__username']
 
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Notification, NotificationAdmin)
--- users/views.py
+++ users/views.py
@@ -22,30 +22,8 @@ def register(request):
 
 @login_required
 def profile(request):
-    if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST,
-                                   request.FILES,
-                                   instance=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            previous_wallet_balance = request.user.profile.wallet  # Store the previous wallet balance
-            p_form.save()
-
-            # Check if the wallet balance has changed
-            if previous_wallet_balance != request.user.profile.wallet:
-                # Create a notification for the user about the wallet balance change
-                Notification.objects.create(
-                    user=request.user,
-                    message=f'Your wallet balance has been updated. New balance: {request.user.profile.wallet}'
-                )
-
-            messages.success(request, f'Your account has been updated!')
-            return redirect('profile')
-
-    else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
+    u_form = UserUpdateForm(instance=request.user)
+    p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'u_form': u_form,
@@ -53,7 +31,6 @@ def profile(request):
     }
 
     return render(request, 'users/profile.html', context)
-
 @login_required
 def notifications(request):
     user_notifications = Notification.objects.filter(user=request.user).order_by('-timestamp')
