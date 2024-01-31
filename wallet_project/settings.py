6e145fb165f0b46db0cab043e65d03dbb6d85715
itachi1010 <onahben977@gmail.com>
2024-01-30 23:10:45 -1100

    database

--- wallet_project/settings.py
+++ wallet_project/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'wallet_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+   #     'ENGINE': 'django.db.backends.sqlite3',
+  #      'NAME': BASE_DIR / 'db.sqlite3',
+ #   }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'foerphvb',  # database name
+        'USER': 'foerphvb',  # username
+        'PASSWORD': 'BAMzARv8LsllX_S4b3ko58Zqs8CObDXe',  # password
+        'HOST': 'jelani.db.elephantsql.com',  # server
+        'PORT': '5432',  # default postgres port
     }
 }
 
